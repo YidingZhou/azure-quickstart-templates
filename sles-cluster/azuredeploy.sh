d0ac87952d90ef44ba8aed19295775887802867d
Yiding Zhou <yiding.zhou@microsoft.com>
2015-07-02 17:11:50 -0700

    sles-cluster template.

---
+++ sles-cluster/azuredeploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# This script can be found on https://github.com/Azure/azure-quickstart-templates/blob/master/slurm/azuredeploy.sh
+# This script is part of azure deploy ARM template
+# This script assumes the Linux distribution to be Ubuntu (or at least have apt-get support)
+# This script will install SLURM on a Linux cluster deployed on a set of Azure VMs
+
+# Basic info
+date > /tmp/azuredeploy.log.$$ 2>&1
+whoami >> /tmp/azuredeploy.log.$$ 2>&1
+echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
+
+# Usage
+if [ "$#" -ne 9 ]; then
+  echo "Usage: $0 MASTER_NAME MASTER_IP WORKER_NAME WORKER_IP_BASE WORKER_IP_START NUM_OF_VM ADMIN_USERNAME ADMIN_PASSWORD TEMPLATE_BASE" >> /tmp/azuredeploy.log.$$
+  exit 1
+fi
+
+# Preparation steps - hosts and ssh
+###################################
+
+# Parameters
+MASTER_NAME=$1
+MASTER_IP=$2
+WORKER_NAME=$3
+WORKER_IP_BASE=$4
+WORKER_IP_START=$5
+NUM_OF_VM=$6
+ADMIN_USERNAME=$7
+ADMIN_PASSWORD=$8
+TEMPLATE_BASE=$9
+
+exit 0
